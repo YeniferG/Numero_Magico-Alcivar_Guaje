1a01f67112fe07b220b7c01845082649383f36af
kevealci <kevealci@espol.edu.ec>
2021-11-17 18:15:00 -0500

    Refactorizacion de codigo para la version 1.1 del Numero Magico

--- NumeroMagico.js
+++ NumeroMagico.js
@@ -1,29 +1,45 @@
-//Numero maximo
-const max = 100;
-//Numero minimo
-const min = 1;
+function clic() {
+  //Numero maximo
+  const max = 100;
+  //Numero minimo
+  const min = 1;
 
-//Numero aleatorio
-let numero_secreto = Math.random() * (max - min) + min;
-numero_secreto = parseInt(numero_secreto);
-console.log(numero_secreto);
+  //Numero aleatorio
+  let numero_secreto = Math.random() * (max - min) + min;
 
-let mensaje = "Ingresa un numero para adivinar el numero magico";
+  //Elegimos la parte entera del numero
+  numero_secreto = parseInt(numero_secreto);
 
-let intentos = 3;
+  // Seteamos el numero de intentos
+  let intentos = 5;
 
-while (intentos > 0) {
-    let numero_del_usuario = prompt(mensaje, "0");
+  //Declaramos una bandera para saber si se gana o se pierde
+  let ganaste = false;
+
+  //Mensaje inicial del juego
+  let mensaje = `Ingresa un numero para adivinar el numero magico\nTienes ${intentos} intentos.`;
+
+  // Ciclo para hacer los intentos del juego
+  while (intentos > 0) {
+    let numero_del_usuario = prompt(mensaje, '0');
     numero_del_usuario = parseInt(numero_del_usuario);
     if (numero_del_usuario === numero_secreto) {
-        alert("Ganaste!!! Adivinaste el numero secreto");
-        break;
+      ganaste = true;
+      break;
     } else if (numero_del_usuario === 0) {
-        break;
+      break;
     }
-    (numero_del_usuario > numero_secreto) ? mensaje = "Lo sentimos, el numero que ingresaste es mayor al numero magico": null;
-    (numero_del_usuario < numero_secreto) ? mensaje = "Lo sentimos, el numero que ingresaste es menor al numero magico": null;
     intentos--;
-}
+    numero_del_usuario > numero_secreto
+      ? (mensaje = `Lo sentimos, el numero que ingresaste es mayor al numero magico\nTe quedan ${intentos} intentos`)
+      : null;
+    numero_del_usuario < numero_secreto
+      ? (mensaje = `Lo sentimos, el numero que ingresaste es menor al numero magico\nTe quedan ${intentos} intentos`)
+      : null;
+  }
 
-alert("Has superado los intentos permitidos ¡GAME OVER!");
+  // Se verifica si gano o perdio
+  ganaste
+    ? alert('Ganaste!!! Adivinaste el numero secreto')
+    : alert('Has superado los intentos permitidos ¡GAME OVER!');
+}
